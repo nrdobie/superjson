ead8fdacbc10b27ecc4f2521e77eed97722bb558
Nicholas Dobie <Nicholas.Dobie@target.com>
2019-12-09 11:49:07 -0600

    chore: cleaned up old values

--- packages/@onedeadpixel/envinfo/presets.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  superjson: {
-    System: ['OS'],
-    Binaries: ['Node', 'Yarn', 'npm'],
-    Browsers: ['Chrome', 'Safari', 'Firefox', 'Edge'],
-    npmPackages: ['@onedeadpixel/superjson']
-  }
-}
